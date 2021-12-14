73a081f31e7893ecaaf102c814fa12fb239bc84a
mdannyc99 <daniapp12@gmail.com>
2021-12-14 21:13:57 +0100

    fix: modified serverUrl in prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,7 +13,7 @@ import { env } from './.env';
 export const environment = {
   production: false,
   version: env.npm_package_version + '-dev',
-  serverUrl: 'https://micro-shortener-dani-main.herokuapp.com/',
+  serverUrl: 'http://localhost:8000',
   defaultLanguage: 'en-US',
   supportedLanguages: ['en-US', 'es-ES'],
 };
